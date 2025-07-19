935d4ebf271c8db8100a3b9559887266f8b089e1
Arslan Khalid <arslan@tuli.com>
2025-07-20 00:40:46 +0500

    New: update pricing section

--- src/app/components/PricingMatrixSection.tsx
+++ src/app/components/PricingMatrixSection.tsx
@@ -1,168 +1,195 @@
-type RowDataKey = "frequency" | "monthly" | "bimonthly" | "weekly";
+"use client";
+import { useState } from "react";
 
-interface ColumnData {
-  header: string;
-  key: RowDataKey;
-}
+const auditFrequencies: string[] = ["monthly", "bimonthly", "weekly"];
 
-const columns: ColumnData[] = [
-  {
-    header: "Audit frequency",
-    key: "frequency",
-  },
-  {
-    header: "Monthly",
-    key: "monthly",
-  },
-  {
-    header: "Bimonthly",
-    key: "bimonthly",
-  },
-  {
-    header: "Weekly",
-    key: "weekly",
-  },
-];
-
-const rows: Record<RowDataKey, string>[] = [
-  {
-    frequency: "<50 pages",
-    monthly: "$500",
-    bimonthly: "$750",
-    weekly: "$1000",
-  },
-  {
-    frequency: "50-75 pages",
-    monthly: "$750",
-    bimonthly: "$1125",
-    weekly: "$1500",
-  },
-  {
-    frequency: "75-100 pages",
-    monthly: "$1000",
-    bimonthly: "$1500",
-    weekly: "$2000",
-  },
-  {
-    frequency: "100-150 pages",
-    monthly: "$1500",
-    bimonthly: "$2250",
-    weekly: "$3000",
-  },
-  {
-    frequency: "150-200 pages",
-    monthly: "$2000",
-    bimonthly: "$3000",
-    weekly: "$4000",
-  },
-  {
-    frequency: "200-250 pages",
-    monthly: "$2500",
-    bimonthly: "$3750",
-    weekly: "$5000",
-  },
-  {
-    frequency: "250-300 pages",
-    monthly: "$3000",
-    bimonthly: "$4500",
-    weekly: "$6000",
-  },
-  {
-    frequency: "300-400 pages",
-    monthly: "$3500",
-    bimonthly: "$5250",
-    weekly: "$7000",
-  },
-  {
-    frequency: "400-500 pages",
-    monthly: "$4000",
-    bimonthly: "$6000",
-    weekly: "$8000",
-  },
-  {
-    frequency: "500+ pages",
-    monthly: "Custom Quote",
-    bimonthly: "Custom Quote",
-    weekly: "Custom Quote",
-  },
-];
+const numberOfPages = [50, 200, 350, 500];
 
 export default function PricingMatrixSection() {
+  const [pages, setPages] = useState("50");
+  const [frequency, setFrequency] = useState("1");
+
+  const calculateAuditPrice = () => {
+    const numberOfPages = Number(pages);
+    const auditFrequency = Number(frequency);
+
+    let totalAmount = numberOfPages * 10;
+
+    if (auditFrequency === 2) {
+      totalAmount = totalAmount * 1.5;
+    }
+
+    if (auditFrequency === 3) {
+      totalAmount = totalAmount * 2;
+    }
+
+    const formattedAmount = totalAmount.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+
+    return formattedAmount;
+  };
+
   return (
-    <div id="pricing" className="bg-white">
+    <div id="pricing" className="bg-stone-50">
       <div className="relative isolate px-6 py-24 sm:py-32 lg:px-8">
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          />
-        </div>
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:text-center">
             <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
               Our pricing matrix
             </h2>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Our pricing depends on two things only: your website&apos;s size and the audit frequency.
+              Our pricing depends on two things only: your website&apos;s size
+              and the audit frequency.
             </p>
           </div>
-          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none overflow-auto">
-            <table className="table-fixed">
-              <thead>
-                <tr>
-                  <th className="border bg-gray-50"></th>
-                  {columns.map((column) => (
-                    <th
-                      key={column.key}
-                      className="text-gray-900 w-1/4 border p-4 text-left  bg-gray-50"
-                    >
-                      {column.header}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {rows.map((row, index) => (
-                  <tr key={`pm-${index}`}>
-                    {index === 0 && (
-                      <td
-                        rowSpan={rows.length}
-                        className="text-gray-900 border p-4 font-bold bg-gray-50"
-                      >
-                        No. of pages
-                      </td>
-                    )}
-                    {columns.map((column: ColumnData) => (
-                      <td
-                        className="text-gray-900 border p-4"
-                        key={`${column.key}-${index}`}
-                      >
-                        {row[column.key]}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none grid grid-cols-1 gap-12 md:grid-cols-2">
+            <div className="flex flex-col py-8 gap-y-12">
+              <h3 className="text-gray-600 text-base">
+                Choose number of pages and audit frequency for your website:
+              </h3>
+              <div className="flex flex-col gap-y-2">
+                <div className="flex justify-between">
+                  <span className="text-gray-600 font-semibold">
+                    Number of pages
+                  </span>
+                  <span className="text-indigo-700">
+                    {pages === "500" ? "500+" : pages}
+                  </span>
+                </div>
+                <div className="w-full">
+                  <input
+                    type="range"
+                    min={50}
+                    max="500"
+                    className="range range-xs range-primary w-full"
+                    step="25"
+                    onChange={(e) => setPages(e.target.value)}
+                    value={pages}
+                  />
+                  <div className="flex justify-between mt-1 text-xs">
+                    {numberOfPages.map((page) => {
+                      const isPageRangePassed = Number(pages) >= page;
+                      return (
+                        <div
+                          key={page}
+                          className="flex flex-col gap-y-2 items-center"
+                        >
+                          <span
+                            className={
+                              isPageRangePassed
+                                ? "text-indigo-400 font-bold"
+                                : "text-gray-400"
+                            }
+                          >
+                            |
+                          </span>
+                          <span
+                            className={
+                              isPageRangePassed
+                                ? "text-indigo-600"
+                                : "text-gray-600"
+                            }
+                          >
+                            {page === 500 ? "500+" : page}
+                          </span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col gap-y-4">
+                <div className="flex justify-between">
+                  <span className="text-gray-600 font-semibold">
+                    Audit frequency
+                  </span>
+                  <span className="text-indigo-700 capitalize">
+                    {auditFrequencies[Number(frequency) - 1]}
+                  </span>
+                </div>
+                <div className="w-full">
+                  <input
+                    type="range"
+                    min={1}
+                    max="3"
+                    className="range range-xs range-primary w-full"
+                    step="1"
+                    onChange={(e) => setFrequency(e.target.value)}
+                    value={frequency}
+                  />
+                  <div className="flex justify-between mt-1 text-xs">
+                    {auditFrequencies.map((auditFrequency) => {
+                      const isSelected =
+                        auditFrequencies[Number(frequency) - 1] ===
+                        auditFrequency;
+                      return (
+                        <div
+                          key={auditFrequency}
+                          className="flex flex-col gap-y-2 items-center"
+                        >
+                          <span
+                            className={
+                              isSelected
+                                ? "text-indigo-400 font-bold"
+                                : "text-gray-400"
+                            }
+                          >
+                            |
+                          </span>
+                          <span
+                            className={
+                              isSelected
+                                ? "text-indigo-600 capitalize"
+                                : "text-gray-600 capitalize"
+                            }
+                          >
+                            {auditFrequency}
+                          </span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="bg-white border p-8 rounded-lg">
+              <div className="flex flex-col items-center gap-y-4">
+                <span className="text-gray-600 text-sm">
+                  Your website audit price
+                </span>
+                <span className="text-gray-900 text-6xl font-semibold">
+                  {calculateAuditPrice()}
+                </span>
+              </div>
+              <hr className="mt-10" />
+              <div className="flex justify-between mt-4">
+                <span className="text-gray-600 font-medium">
+                  Number of pages
+                </span>
+                <span className="text-gray-600">
+                  {pages === "500" ? "500+" : pages}
+                </span>
+              </div>
+              <hr className="mt-4" />
+              <div className="flex justify-between mt-4">
+                <span className="text-gray-600 font-medium">
+                  Audit frequency
+                </span>
+                <span className="text-gray-600 capitalize">
+                  {auditFrequencies[Number(frequency) - 1]}
+                </span>
+              </div>
+              <hr className="mt-4" />
+              <button
+                onClick={() => {}}
+                className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-4 text-center text-md text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Start my SEO audit
+              </button>
+            </div>
           </div>
-        </div>
-        <div
-          aria-hidden="true"
-          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          />
         </div>
       </div>
     </div>
